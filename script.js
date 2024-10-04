392f6b4230f5d57792f1c37d0df62d718f3b1d41
Rickard Hultgren <sababa.sababa@gmail.com>
2024-10-04 18:19:22 +0000

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateDateTime() {
+	const datetimeElement = document.getElementById('datetime');
+	const now = new Date();
+	datetimeElement.textContent = now.toLocaleString();
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime(); // Call immediately to avoid delay
