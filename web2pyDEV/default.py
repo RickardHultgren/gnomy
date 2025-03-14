eb55694b9992d2287e8244edcb5f0896fdbd9ac4
Rickard Hultgren <sababa.sababa@gmail.com>
2025-03-14 12:27:49 +0100

    update

--- web2pyDEV/default.py
+++ web2pyDEV/default.py
@@ -45,7 +45,6 @@ def index():
                                            ))
     ]
     # Create links for each pond that will pass the pond ID in the URL when clicked
-    response.js= ("alert('%s');" % row.id)
     # Grid for displaying ponds
     pond_grid = SQLFORM.grid(pond_query, fields=pond_fields, links=pond_links, create=False, editable=False, deletable=False,
                              details=False, paginate=10, csv=False, user_signature=False)
