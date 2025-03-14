718ab8375557335c5f977d1cc5fa692d5ad4fb55
Rickard Hultgren <sababa.sababa@gmail.com>
2025-03-14 05:31:31 +0100

    update

--- web2pyDEV/default.py
+++ web2pyDEV/default.py
@@ -38,19 +38,24 @@ def index():
                              details=False, paginate=10, csv=False, user_signature=False)
 
     # ---- Rootstocks Section ----
-    db.rootstock.created_by.default = auth.user.id
-    db.rootstock.created_by.writable = False
-    db.rootstock.created_by.readable = False
-
-    # Form for adding rootstocks
-    rootstock_form = SQLFORM(db.rootstock).process()
-
-    # ✅ Show rootstocks only if a pond is selected
     selected_pond_id = request.vars.selected_pond_id
+
+    # If a pond is selected, set up the rootstock form for that pond
     if selected_pond_id:
+        selected_pond = db.pond(selected_pond_id) or redirect(URL('index'))
+
+        db.rootstock.pond.default = selected_pond.id
+        db.rootstock.pond.writable = False  # Hide pond field in form
+        db.rootstock.pond.readable = False
+        db.rootstock.created_by.default = auth.user.id
+        db.rootstock.created_by.writable = False
+        db.rootstock.created_by.readable = False
+
+        rootstock_form = SQLFORM(db.rootstock).process()
         rootstock_query = (db.rootstock.pond == selected_pond_id)
     else:
-        rootstock_query = (db.rootstock.id == None)  # No rootstocks will be shown
+        rootstock_form = None  # No form shown if no pond selected
+        rootstock_query = (db.rootstock.id == None)  # No rootstocks displayed
 
     rootstock_fields = [db.rootstock.name]
     rootstock_links = [
@@ -61,12 +66,58 @@ def index():
     rootstock_grid = SQLFORM.grid(rootstock_query, fields=rootstock_fields, links=rootstock_links, create=False, editable=False, deletable=False,
                                   details=False, paginate=10, csv=False, user_signature=False)
 
-    return dict(message=None, form=form, grid=pond_grid, rootstock_form=rootstock_form, rootstock_grid=rootstock_grid, pond_container=DIV(), rootstock_container=DIV())
+    return dict(message=None, form=form, grid=pond_grid, rootstock_form=rootstock_form, rootstock_grid=rootstock_grid, 
+                pond_container=DIV(), rootstock_container=DIV())
 
 
-def rootstock():
-    """ Displays a selected rootstock's details. """
-    rootstock_id = request.args(0, cast=int)
-    rootstock = db.rootstock(rootstock_id) or redirect(URL('index'))
 
-    return dict(rootstock=rootstock)
+
+
+
+# ---- API (example) -----
+@auth.requires_login()
+def api_get_user_email():
+    if not request.env.request_method == 'GET': raise HTTP(403)
+    return response.json({'status':'success', 'email':auth.user.email})
+
+# ---- Smart Grid (example) -----
+@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+def grid():
+    response.view = 'generic.html' # use a generic view
+    tablename = request.args(0)
+    if not tablename in db.tables: raise HTTP(403)
+    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    return dict(grid=grid)
+
+# ---- Embedded wiki (example) ----
+def wiki():
+    auth.wikimenu() # add the wiki to the menu
+    return auth.wiki()
+
+# ---- Action for login/register/etc (required for auth) -----
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+# ---- action to server uploaded static content (required) ---
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
