20744bc462538539f00012479202b8360af23b79
Rickard Hultgren <sababa.sababa@gmail.com>
2025-03-19 07:40:37 +0100

    upgrade

--- web2pyDEV/default.py
+++ web2pyDEV/default.py
@@ -78,7 +78,7 @@ def add_tendril():
         return response.json({"status": "error", "error": "Invalid rootstock ID"})
 
     tendril_id = db.tendril.insert(
-        rootstock=rootstock_id,
+        rootstock=session.rootstock_id,
         knotstock=knotstock_id if knotstock_id else None,  # Allow nullable values
         name=tendril_name,
         carry=tendril_carry,
